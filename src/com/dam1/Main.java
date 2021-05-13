f2455fc6ae2c130076aa08cbc44f827465cf7a77
plagocastro <plagocastro@danielcastelao.org>
2021-05-13 08:56:09 +0200

    C

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // ----
         // varios idomas implementados
         // fix
-        System.out.println("B");
+        System.out.println("C");
         // trabajando en la nueva funcionalidad
 
         // terminada la nueva funcionalidad
